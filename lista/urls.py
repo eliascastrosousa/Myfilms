ab508daef1efdec4be2ee5f06231333136592489
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2023-04-01 21:25:55 -0300

    git commit crud

--- lista/urls.py
+++ lista/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, criaritem, salvar
+from .views import home, criaritem, salvar,update, delete,editar, excluir
 
 urlpatterns = [
     path('', home, name="index"),
-    path('criaritem/', criaritem, name="criaritem"),
+    path('criaritem', criaritem, name="criaritem"),
     path('salvar/', salvar, name="salvar"),
+    path('editar/<int:id>', editar, name="editar"),
+    path('update/<int:id>', update, name="update"),
+    path('delete/<int:id>', delete, name="delete"),
+    path('excluir/<int:id>', excluir, name="excluir"),
 
     
 
--- lista/views.py
+++ lista/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Lista
 
 def home(request):
@@ -11,8 +11,42 @@ def criaritem(request):
 def salvar(request):
     v_nome = request.POST.get("nome")
     v_tipo_filme = request.POST.get("tipo_filme")
-    v_concluido =  request.POST.get("concluido")
+    
+    if request.POST.get("concluido"):
+        v_concluido = True
+    else:
+        v_concluido = False
+
     v_resenha =  request.POST.get("resenha")
     v_lista = Lista.objects.all()
     Lista.objects.create(nome=v_nome, tipo_filme = v_tipo_filme, concluido = v_concluido, resenha = v_resenha )
     return render(request, 'index.html', {"lista":v_lista})
+
+def editar(request, id):
+    lista  = Lista.objects.get(id=id)
+    return render(request, 'update.html', {'lista':lista})
+
+def update(request, id):
+    v_nome = request.POST.get("nome")
+    v_tipo_filme = request.POST.get("tipo_filme")
+    if request.POST.get("concluido"):
+        v_concluido = True
+    else:
+        v_concluido = False
+    v_resenha =  request.POST.get("resenha")
+    lista = Lista.objects.get(id=id)
+    lista.nome = v_nome
+    lista.tipo_filme = v_tipo_filme
+    lista.concluido = v_concluido
+    lista.resenha = v_resenha
+    lista.save()
+    return redirect(home)   
+
+def delete(request, id):
+    lista = Lista.objects.get(id=id)
+    return render(request, 'delete.html', {"lista":lista})
+
+def excluir(request, id):
+    lista = Lista.objects.get(id=id)
+    lista.delete()
+    return redirect(home)
